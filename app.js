c4d0afd98840087d177d58c295df6b1ac306078a
danilo-89 <danilo89abcd@gmail.com>
2024-02-21 12:03:18 +0100

    fix lint errors

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-	res.send('5'); // change this string to ensure a new version deployed
-});
+  res.send('5') // change this string to ensure a new version deployed
+})
 
 app.get('/health', (req, res) => {
-	// eslint-disable-next-line no-constant-condition
-	// if (true) throw('error...  ')
-	res.send('ok');
-});
+// eslint-disable-next-line no-constant-condition
+// if (true) throw('error...  ')
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
-	// eslint-disable-next-line no-console
-	console.log(`server started on port ${PORT}`);
-});
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${PORT}`)
+})
